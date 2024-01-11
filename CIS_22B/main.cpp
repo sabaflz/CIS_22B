258328d9f8df8ae4848fa7e80b461eb2ec46ee86
sabaflz <sabaa.feilizadeh@gmail.com>
2024-01-10 17:38:20 -0800

    Initial Commit

---
+++ CIS_22B/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS_22B
+//
+//  Created by Saba Flz on 1/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
